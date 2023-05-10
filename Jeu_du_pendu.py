8ae712d4f17b24f66ef0ef9d22526cada338dcf6
P3PiT0 <lucas.chz@laposte.net>
2023-05-10 15:55:08 -0400

    Projet MGA802

---
+++ Jeu_du_pendu.py
@@ -0,0 +1,8 @@
+import random
+
+#FONCTION D'IMPORTATION D'UN MOT
+def choix_mot ():
+    with open("mots_pendu.txt", 'r') as f:
+        liste_mots = list(map(str, f.read().split()))                 
+        return random.choice(liste_mots)
+
