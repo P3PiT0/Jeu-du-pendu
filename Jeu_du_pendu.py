537a0929fccd763f0f3bb14ef90765c28242480e
P3PiT0 <lucas.chz@laposte.net>
2023-05-10 15:58:25 -0400

    Choix du mot OK

--- Jeu_du_pendu.py
+++ Jeu_du_pendu.py
@@ -5,4 +5,5 @@ def choix_mot ():
     with open("mots_pendu.txt", 'r') as f:
         liste_mots = list(map(str, f.read().split()))                 
         return random.choice(liste_mots)
-
+        
+print(choix_mot())
