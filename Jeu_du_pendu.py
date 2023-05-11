f16cc1f3c0244497c6c11d5c496800fa5b564ab2
P3PiT0 <lucas.chz@laposte.net>
2023-05-11 10:57:28 -0400

    Update procedure jeu et affichage interface

--- Jeu_du_pendu.py
+++ Jeu_du_pendu.py
@@ -6,27 +6,54 @@ def choix_mot ():
     with open("mots_pendu.txt", 'r') as f:
         liste_mots = list(map(str, f.read().split()))                 
         return random.choice(liste_mots)
+    
+#FONCTION QUI TEST SI LA LETTRE CHOISIE EST DANS LE MOT
+def Trouve (mot, lettre): 
+    if mot.count(lettre) !=0 : 
+        return True
+    else: 
+        return False
+    
+def afficher_mot(mot):
+    for i in mot :
+        print(i,end=" ") 
+    print("\n")
         
 #PROCEDURE DE JEU 
 def jeu_pendu (mot):
+    print('Bienvenue dans le jeu du pendu. Tu as 6 chances pour trouver le mot')
     vie = 6 
     win = False
-    #mot_trouve
+    mot_trouve=[]
+    for i in mot:
+        mot_trouve+=["__"]
     alphabet_restant = list(string.ascii_lowercase)
+    #print(mot)
+    
     while vie > 0 and not win : 
         lettre = (input("Choisissez une lettre : "))    
         if alphabet_restant.count(lettre) == 0 : 
-            print('La lettre a déjà été choisi, rééssayer avec une des lettres restantes : ')
-            print(alphabet_restant)
-            
+            print('La lettre a déjà été choisie, rééssayer avec une des lettres restantes : ')
+            #print(alphabet_restant)
+            afficher_mot(alphabet_restant)
         else: 
             alphabet_restant.remove(lettre)
-
-            '''for i in mot: 
-                if lettre == i : 
-                    #CAS 1 toutes lettres trouvée 
-                    #CAS 2 reste une lettre a trouver '''
-                        
+            if Trouve(mot, lettre) : #La lettre est dans le mot
+                for i in range(0,len(mot)): 
+                    if mot[i]==lettre: 
+                        mot_trouve[i]=lettre    
+                print(f"La lettre {lettre} est dans le mot. {vie} vie(s) restante(s)")
+                afficher_mot(mot_trouve)
+                if mot_trouve.count('__')==0:
+                    win=True
+            else: #La lettre n'est pas dans le mot                
+                vie-=1
+                print(f"La lettre {lettre} n'est pas dans le mot, {vie} vie(s) restante(s)")
+                    
+    if vie <= 0 : 
+        print(f"Perdu, le mot était {mot}, recommence")
+    else : 
+        print(f"Bravo, le mot était bien {mot}, CHAMPION")                   
     
 #CORPS DU PROGRAMME
 jeu_pendu(choix_mot())
